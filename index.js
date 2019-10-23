ece8da2118f038d2d6182a780ea96cc4569debc3
hanrougang <hanrougang@sensetime.com>
2019-10-23 17:31:43 +0800

    initialize

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  exec.exec('ansible-playbook --version');
 } catch (error) {
   core.setFailed(error.message);
 }
