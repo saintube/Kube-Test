8c6ba2e768ebc50aeddd4d04b7880bd9de227540
hanrougang <hanrougang@sensetime.com>
2019-10-23 18:46:23 +0800

    initialize

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const exec = require('@actions/exec');
 
 try {
   (async() => {
-    
+    console.log("Kube Test Action is running...");
     await exec.exec('git', ['clone', 'https://github.com/saintube/Kube-Test.git']);
+    console.log("DEBUG:");
     await exec.exec('ls Kube-Test/lib');
     await exec.exec('sudo touch /mnt/parameters.yml');
+    await exec.exec('sudo echo "[local]" >> /etc/ansible/hosts')
+    await exec.exec('sudo echo "127.0.0.1" >> /etc/ansible/hosts')
     await exec.exec('ansible-playbook', ['-v', 'Kube-Test/lib/ci.yml']);
-
   })();
 } catch (error) {
   core.setFailed(error.message);
