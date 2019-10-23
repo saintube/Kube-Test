a0c4374fa475cdf427cec3a98b5e44c923ba7115
hanrougang <hanrougang@sensetime.com>
2019-10-23 18:37:29 +0800

    initialize

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ try {
   (async() => {
     
     await exec.exec('git', ['clone', 'https://github.com/saintube/Kube-Test.git']);
-    await exec.exec('ls Kube-Test');
+    await exec.exec('ls Kube-Test/lib');
+    await exec.exec('sudo touch /mnt/parameters.yml');
     await exec.exec('ansible-playbook', ['-v', 'Kube-Test/lib/ci.yml']);
 
   })();
