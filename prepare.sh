afca9dd1d4ec06a713835d34a364986dff07ee38
wangxuhao <wangxuhao@sensetime.com>
2019-10-24 15:43:12 +0800

    Modify index.js

--- prepare.sh
+++ prepare.sh
@@ -3,3 +3,9 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )";
 kubeconfig=(`kind get kubeconfig-path`)
 echo "kube_config: \"$kubeconfig\"" > env.yml
+
+ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml >> log
+
+ls $GITHUB_WORKSPACE/
+
+cat log
