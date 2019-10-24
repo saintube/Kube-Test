6e999ba57043f81a6e2105ffd77f0422be953d50
wangxuhao <wangxuhao@sensetime.com>
2019-10-24 16:31:23 +0800

    Modify path

--- index.js
+++ index.js
@@ -5,31 +5,12 @@ const exec = require('@actions/exec');
 (async() => {
     try {
     console.log("Kube Test Action is running...");
-    await exec.exec('git', ['clone', '-b', 'output', 'https://github.com/saintube/Kube-Test.git']);
+    await exec.exec('git', ['clone', 'https://github.com/saintube/Kube-Test.git']);
     await exec.exec('sudo touch /mnt/parameters.yml');
     await exec.exec('sudo echo "[local]" >> /etc/ansible/hosts');
     await exec.exec('sudo echo "127.0.0.1" >> /etc/ansible/hosts');
     await exec.exec('bash ./Kube-Test/prepare.sh');
-
-    // let myOutput = '';
-    // let myError = '';
-
-    // const options = {};
-    // options.listeners = {
-    //   stdout: (data: Buffer) => {
-    //     myOutput += data.toString();
-    //   },
-    //   stderr: (data: Buffer) => {
-    //     myError += data.toString();
-    //   }
-    // };
-    await exec.exec('ansible-playbook', ['-v', './.kubetest/ci.yml']);
-
-    // console.log("Print stdout and stderr")
-
-    // console.log(myOutput);
-    // console.log(myError);
-    
+    //await exec.exec('ansible-playbook', ['-v', './.kubetest/ci.yml'])
   } catch (error) {
     core.setFailed(error.message);
   }
