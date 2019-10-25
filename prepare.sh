013bbbd12bfb911aa5424b473d20cada94327842
wangxuhao <wangxuhao@sensetime.com>
2019-10-25 15:23:35 +0800

    Add UI

--- prepare.sh
+++ prepare.sh
@@ -4,8 +4,18 @@ cd "$( dirname "${BASH_SOURCE[0]}" )";
 kubeconfig=(`kind get kubeconfig-path`)
 echo "kube_config: \"$kubeconfig\"" > env.yml
 
-ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml
+ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml >> demo.log
+cat demo.log
+cp $GITHUB_WORKSPACE/.kubetest/*.yaml ./
+
+cp ./Kube-Test/kubeTestMarkdownGen.py ./
+
+python3 kubeTestMarkdownGen.py
+
+cp dashboardData.json src/dashboardData/
+cp -r posts/* src/posts/
+
 
 #ls $GITHUB_WORKSPACE/
 
-#cat log
+
