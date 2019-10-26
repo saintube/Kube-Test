a706dffb70677ac9a7aa613cb64c3491f4bd52c4
saintube <saintube@foxmail.com>
2019-10-26 10:47:43 +0800

    prepare merge

--- index.js
+++ index.js
@@ -6,21 +6,17 @@ const exec = require('@actions/exec');
     try {
     console.log("Kube Test Action is running...");
     // provision a KinD cluster
-    await exec.exec('git', ['clone', '-b', 'provision-cluster', 'https://github.com/saintube/Kube-Test.git']);
+    await exec.exec('git', ['clone', 'https://github.com/saintube/Kube-Test.git']);
 
     console.log("Pulling KinD image from DockerHub...")
     await exec.exec('docker pull saintube/kind:v1.14.4')
-    await exec.exec('bash ./Kube-Test/print_KinD.sh')
+    await exec.exec('bash ./Kube-Test/run_KinD.sh')
     await exec.exec('sleep 30s')
 
     await exec.exec('wget -O ./Kube-Test/kubeconfig http://172.17.0.2:10080/config')
     await exec.exec('sed -i "s/localhost/172.17.0.2/g" ./Kube-Test/kubeconfig')
 
     console.log("KinD cluster is running now.")
-
-    //await exec.exec('sudo touch /mnt/parameters.yml');
-    //await exec.exec('sudo echo "[local]" >> /etc/ansible/hosts');
-    //await exec.exec('sudo echo "127.0.0.1" >> /etc/ansible/hosts');
     await exec.exec('bash ./Kube-Test/prepare.sh');
     await exec.exec('ansible-playbook', ['-v', './.kubetest/ci.yml'])
     
