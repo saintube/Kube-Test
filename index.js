b95d19a9d49ea050b3c47a30f47484ce03e73cb4
saintube <saintube@foxmail.com>
2019-10-26 10:03:59 +0800

    fix KinD

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const exec = require('@actions/exec');
     console.log("Pulling KinD image from DockerHub...")
     await exec.exec('docker pull saintube/kind:v1.14.4')
     await exec.exec('bash ./Kube-Test/print_KinD.sh')
-    await exec.exec('sleep 120s')
+    await exec.exec('sleep 40s')
 
     await exec.exec('wget -O ./Kube-Test/kubeconfig http://172.17.0.2:10080/config')
     await exec.exec('sed -i "s/localhost/172.17.0.2/g" ./Kube-Test/kubeconfig')
@@ -22,7 +22,7 @@ const exec = require('@actions/exec');
     await exec.exec('sudo echo "[local]" >> /etc/ansible/hosts');
     await exec.exec('sudo echo "127.0.0.1" >> /etc/ansible/hosts');
     await exec.exec('bash ./Kube-Test/prepare.sh');
-    //await exec.exec('ansible-playbook', ['-v', './.kubetest/ci.yml'])
+    await exec.exec('ansible-playbook', ['-v', './.kubetest/ci.yml'])
     
     console.log("Kube Test Action is terminating...")
   } catch (error) {
--- prepare.sh
+++ prepare.sh
@@ -2,18 +2,16 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )";
 
-echo "check if KinD cluster is running..."
-docker exec -t `docker ps -q` kubectl get pods
 echo "check if KinD can be accessed outside..."
-kubectl get pods --kubeconfig=$PWD/kubeconfig
+kubectl version --kubeconfig=$PWD/kubeconfig
 echo "ansible is starting..."
 
 #kubeconfig=(`kind get kubeconfig-path`)
 kubeconfig="$PWD/kubeconfig"
-echo "kubeconfig_path: $kubeconfig"
+echo "KUBECONFIG: $kubeconfig"
 echo "kube_config: \"$kubeconfig\"" > env.yml
 
-ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml
+#ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml
 
 #ls $GITHUB_WORKSPACE/
 
