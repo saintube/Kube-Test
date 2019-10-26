990803e27923a278344f3e3192281cb580cd028c
saintube <saintube@foxmail.com>
2019-10-26 09:31:46 +0800

    fix KinD

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo"check if KinD cluster is running..."
+docker exec -t `docker ps -q` kubectl get pods
+echo "check if KinD can be accessed outside..."
+
 cd "$( dirname "${BASH_SOURCE[0]}" )";
 #kubeconfig=(`kind get kubeconfig-path`)
 kubeconfig="$PWD/kubeconfig"
