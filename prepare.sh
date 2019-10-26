6762cb06daf2f5a94c179dd87edd6538da739119
saintube <saintube@foxmail.com>
2019-10-26 11:17:35 +0800

    fix hardconding

--- prepare.sh
+++ prepare.sh
@@ -9,19 +9,19 @@ kubeconfig="$PWD/kubeconfig"
 echo "KUBECONFIG: $kubeconfig"
 echo "kube_config: \"$kubeconfig\"" > env.yml
 
-ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml >> demo.log
+ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml  2>&1 | tee demo.log
 echo $GITHUB_SHA >> demo.log
 cat demo.log
 cp $GITHUB_WORKSPACE/.kubetest/*.yaml ./
-cat $GITHUB_WORKSPACE/.kubetest/pod-delete-litmus-result.yaml
+cat $GITHUB_WORKSPACE/.kubetest/*.yaml
 
 #cp $GITHUB_WORKSPACE/Kube-Test/kubeTestMarkdownGen.py ./
 
 python3 kubeTestMarkdownGen.py
 ls
 
-cp dashboard.json $GITHUB_WORKSPACE/src/dashboardData/
-cat $GITHUB_WORKSPACE/src/dashboardData/dashboard.json
-cp -r posts/* $GITHUB_WORKSPACE/src/posts/
+#cp dashboard.json $GITHUB_WORKSPACE/src/dashboardData/
+#cat $GITHUB_WORKSPACE/src/dashboardData/dashboard.json
+#cp -r posts/* $GITHUB_WORKSPACE/src/posts/
 
 #ls $GITHUB_WORKSPACE/
