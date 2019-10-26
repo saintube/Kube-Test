38fe9b718c2bfff6a2ab84c2bfaad0bd03154f6d
saintube <saintube@foxmail.com>
2019-10-26 09:45:17 +0800

    fix KinD

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo"check if KinD cluster is running..."
+cd "$( dirname "${BASH_SOURCE[0]}" )";
+
+echo "check if KinD cluster is running..."
 docker exec -t `docker ps -q` kubectl get pods
 echo "check if KinD can be accessed outside..."
+kubectl get pods --kubeconfig=$PWD/kubeconfig
+echo "ansible is starting..."
 
-cd "$( dirname "${BASH_SOURCE[0]}" )";
 #kubeconfig=(`kind get kubeconfig-path`)
 kubeconfig="$PWD/kubeconfig"
+echo "kubeconfig_path: $kubeconfig"
 echo "kube_config: \"$kubeconfig\"" > env.yml
 
 ansible-playbook -v $GITHUB_WORKSPACE/.kubetest/ci.yml
